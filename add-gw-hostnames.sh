a7cf0df91b5a85871b15a759597eefe7e58389c9
Michael Goessmann Matos <dj0nz@users.noreply.github.com>
2023-02-02 13:15:21 +0100

    Add gateway host names to Gaia

--- add-gw-hostnames.sh
+++ add-gw-hostnames.sh
@@ -1,44 +1,51 @@
 #!/bin/bash
 
-# create hosts entries for managed gateways
-# run on check point management server
-# dj0Nz nov 2020
+# Create host name entries for managed gateways
+# Run on Check Point management server
+# dj0Nz feb 2023
 
 . /etc/profile.d/CP.sh
 
-echo "querying check point database. patience please."
+echo "Querying Check Point database. Patience please..."
 GW_LIST=(`mgmt_cli -r true show gateways-and-servers limit 500 offset 0 details-level full --format json | $CPDIR/jq/jq -r '.objects[]|[.["type"], .["name"], .["ipv4-address"]]| @csv' | egrep "cluster-member|simple-gateway" | cut -d "," -f 2,3 | tr -d '"'`)
+IPREGEX="(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
 CHANGED=no
 
 LOCKED=`clish -c "show config-state" | grep owned`
 if [[ $LOCKED ]]; then
-    echo "gaia database locked. exiting."
+    echo "Gaia database locked. Exiting."
     exit 1
 fi
 
-echo "creating missing host name entries."
+echo "Creating missing host name entries."
 for INDEX in "${GW_LIST[@]}"; do
     # remove nonprintable, does not work otherwise
     GW=`tr -dc '[[:print:]]' <<< "$INDEX"`
     GW_NAME=`echo $GW | awk -F , '{print $1}'`
     GW_IP=`echo $GW | awk -F , '{print $2}'`
-    HOSTS_NAME=`clish -c "show configuration host" | grep $GW_NAME | awk '{print $4}'`
-    HOSTS_IP=`clish -c "show configuration host" | grep $GW_NAME | awk '{print $6}'`
-    if [[ "$HOSTS_NAME" = "$GW_NAME" ]]; then
-        if [[ "$HOSTS_IP" = "$GW_IP" ]]; then
-            printf "%-17s %s\n" "$GW_NAME: " "hosts entry exists"
-        else
-            printf "%-17s %s\n" "$GW_NAME: " "hosts entry exists, but ip is different. check manually!"
-        fi
+    CHECK=`echo $GW_IP | grep -E "$IPREGEX"`
+    if [[ "$CHECK" = "" ]]; then
+        printf "%-15s %s\n" "$GW_NAME: " "IP address syntax check failed. Address: $GW_IP"
     else
-        printf "%-17s %s\n" "$GW_NAME: " "creating hosts entry"
-        clish -c "add host name $GW_NAME ipv4-address $GW_IP"
-        CHANGED=yes
+        HOSTS_NAME=`clish -c "show configuration host" | grep $GW_NAME | awk '{print $4}'`
+        HOSTS_IP=`clish -c "show configuration host" | grep $GW_NAME | awk '{print $6}'`
+        if [[ "$HOSTS_NAME" = "$GW_NAME" ]]; then
+            if [[ "$HOSTS_IP" = "$GW_IP" ]]; then
+                printf "%-15s %s\n" "$GW_NAME: " "Host name entry exists"
+            else
+                printf "%-15s %s\n" "$GW_NAME: " "Host name entry exists, but IP is different. Check manually!"
+            fi
+        else
+            printf "%-15s %s\n" "$GW_NAME: " "Creating host name entry"
+            clish -c "add host name $GW_NAME ipv4-address $GW_IP"
+            CHANGED=yes
+        fi
     fi
 done
+
 if [[ "$CHANGED" = "yes" ]]; then
-    echo "saving changed configuration."
+    echo "Saving changed configuration."
     clish -c "save config"
 else
-    echo "configuration unchanged."
+    echo "Configuration unchanged."
 fi
