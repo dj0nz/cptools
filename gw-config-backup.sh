5c15ee98359e5340eef688811cc706b94d51ec3a
Michael Goessmann Matos <dj0nz@users.noreply.github.com>
2023-02-14 08:14:23 +0100

    Extended version with gw info and LOM

--- gw-config-backup.sh
+++ gw-config-backup.sh
@@ -2,37 +2,53 @@
 
 # Check Point Firewall Module Backup
 #
-# Skript nach /home/admin kopieren & chmod 700
-# Die Backups landen dann in dem in $CFG_PATH definierten Verzeichnis
-# Einplanen mit add cron job Modulebackup command "/home/admin/gw-config-backup.sh" recurrence weekly days 0 time 23:30
-# Voraussetzung: Für das zu sichernde Gateway muss bereits ein Hosts-Eintrag existieren.
-# Siehe dazu auch add-gw-hostnames.sh
+# Runs locally on Smartcenter.
+# Copy to desired destination (e.g. /home/admin/scripts) and chmod 700
+# Schedule to run weekly or monthly. Example: 
+# add cron job Modulebackup command "/home/admin/gw-config-backup.sh" recurrence weekly days 0 time 23:30
+# Backups got to $CFG_PATH
 # 
-# Michael Goessmann Matos, NTT - Mar 2021
+# Michael Goessmann Matos, NTT Data - Feb 2023
 
-. /opt/CPshared/5.0/tmp/.CPprofile.sh
+. /etc/profile.d/CP.sh
 
-# Liste von Gateways aus der Check Point Datenbank extrahieren
-GW_LIST=(`mgmt_cli -r true show gateways-and-servers limit 500 | egrep -B 1 'cluster-member|simple-gateway' | grep name | awk '{print $2}' | tr -d '"'`)
+# Get Gateway list (Cluster members only)
+GW_LIST=(`mgmt_cli -r true show gateways-and-servers limit 500 --format json | $CPDIR/jq/jq -r '.objects[]| select(.type | contains("cluster-member"))|.name'`)
 PORT=18208
 CFG_PATH=/home/admin/module-config
 
 if [ ! -d $CFG_PATH ]; then
-   mkdir -p $CFG_PATH
+    mkdir -p $CFG_PATH
 fi
 
 for INDEX in "${GW_LIST[@]}"; do
-   # Nonprintable entfernen, sonst klappt das nicht. Warum auch immer.
-   GW=`tr -dc '[[:print:]]' <<< "$INDEX"`
-   # Gibts fuer das Gateway einen Hosts-Eintrag? Backup nur dann versuchen.
-   HOSTS=`clish -c "show configuration host" | grep $GW | awk '{print $4}'`
-   if [[ "$HOSTS" = "$GW" ]]; then
-      # Config Backup via cprid_util versuchen, wenn der CPRID Port offen ist.
-      OPEN=`timeout 3 bash -c "</dev/tcp/$GW/$PORT" 2>/dev/null && echo "Open" || echo "Closed"`
-      if [[ "$OPEN" = "Open" ]]; then
-         $CPDIR/bin/cprid_util -server $GW -verbose rexec -rcmd clish -c "show configuration" > $CFG_PATH/$GW.cfg
-      else
-         printf "%-17s %s\n" "$GW: " "Unreachable"
-      fi
-   fi
+    # Remove nonprintables, does not work otherwise.
+    GW=`tr -dc '[[:print:]]' <<< "$INDEX"`
+    # Only try to backup if there is a host entry for this gateway.
+    HOSTS=`clish -c "show configuration host" | grep $GW | awk '{print $4}'`
+    if [[ "$HOSTS" = "$GW" ]]; then
+        # Check if CPRID port open.
+        OPEN=`timeout 3 bash -c "</dev/tcp/$GW/$PORT" 2>/dev/null && echo "Open" || echo "Closed"`
+        if [[ "$OPEN" = "Open" ]]; then
+            LOCKED=`$CPDIR/bin/cprid_util -server $GW -verbose rexec -rcmd clish -c "show config-state" | grep owned`
+	        if [[ $LOCKED ]]; then
+                $CPDIR/bin/cprid_util -server $GW -verbose rexec -rcmd clish -c "lock database override" > /dev/null 2>&1
+            fi
+            printf "%-17s %s\n" "$GW: " "Saving configuration backup to $BKPDIR"
+            $CPDIR/bin/cprid_util -server $GW -verbose rexec -rcmd clish -c "show configuration" > $BKPDIR/$GW.cfg
+            $CPDIR/bin/cprid_util -server $GW -verbose rexec -rcmd clish -c "show asset all" > $BKPDIR/$GW.info
+            $CPDIR/bin/cprid_util -server $GW -verbose rexec -rcmd clish -c "show version all" >> $BKPDIR/$GW.info
+            # Optionally get LOM IP (next four lines)
+            # $CPDIR/bin/cprid_util -server $GW -verbose rexec -rcmd service ipmi start > /dev/null
+            # LOMIP=`$CPDIR/bin/cprid_util -server $GW -verbose rexec -rcmd ipmitool lan print 8 | grep 'IP Address' | grep -v Source | awk '{print $4}'`
+            # echo "LOM IP Address: $LOMIP" >> $BKPDIR/$GW.info
+            # $CPDIR/bin/cprid_util -server $GW -verbose rexec -rcmd service ipmi stop > /dev/null
+            echo "" >> $BKPDIR/$GW.info
+            JUMBO=`$CPDIR/bin/cprid_util -server $GW -verbose rexec -rcmd cpinfo -y fw1 2>&1 | grep JUMBO | awk '{print $3}'`
+            echo "Installed Jumbo Take: $JUMBO" >> $BKPDIR/$GW.info
+            echo "" >> $BKPDIR/$GW.info
+        else
+            printf "%-17s %s\n" "$GW: " "Unreachable"
+        fi
+    fi
 done
