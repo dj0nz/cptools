80f789792dd3a98581751ac64eac71560ea766f0
Michael Goessmann Matos <dj0nz@users.noreply.github.com>
2024-05-15 18:25:40 +0200

    Delete VRRP Interface

--- del-cluster-if.sh
+++ del-cluster-if.sh
@@ -4,43 +4,43 @@
 # dj0Nz Mai 2024
 
 # Variablen:
-# PROXYARP = Alle NAT-IPs, für die ein Proxy ARP Eintrag auf dem betreffenden Interface erstellt wurde (show arp proxy ...)
-# LOCALNET = Der Netzwerk-Teil des betreffenden Interfaces (Nur für /24-Netze!) 
-# VLAN und INTERFACE sind ja selbsterklärend...
-PROXYARP="192.168.3.113 192.168.3.116"
-LOCALNET="192.168.16"
-VLAN="16"
-INTERFACE="eth5"
+# LOCALNET = Der Netzwerk-Teil des betreffenden Interfaces
+# VLAN und INTERFACE sind eh klar
+LOCALNET="192.168.101"
+VLAN="101"
+INTERFACE="bond0"
+IPADDR=$(clish -c "show interface $INTERFACE.$VLAN ipv4-address" | awk '{ print $2 }')
 
 # Schritt 1: Konfiguration sichern
-if [[ -f proxyarp.cfg ]]; then
-    rm proxyarp.cfg
-fi
-for IP in $PROXYARP; do
-    clish -c "show configuration arp proxy" | grep $IP >> proxyarp.cfg
-done
+echo "Sichere Konfigurationen..."
+clish -c "show configuration arp proxy" | grep $LOCALNET > proxyarp.cfg
 clish -c "show configuration static-route" | grep $LOCALNET > staticroute.cfg
-clish -c "show configuration interface" | grep $INTERFACE | grep vlan > vlan.cfg
+clish -c "show configuration interface" | grep $INTERFACE | grep vlan | grep $VLAN > vlan.cfg
 clish -c "show configuration interface" | grep $INTERFACE.$VLAN > interface.cfg
 clish -c "show configuration mcvr" | grep $LOCALNET > mcvr.cfg
 
 # Schritt2: Kommandos für das Entfernen "bauen"
 cat proxyarp.cfg | cut -d ' ' -f1-5 | sed 's/add/delete/' > proxyarp.del
 cat staticroute.cfg | sed 's/on/off/' > staticroute.del
-cat mcvr.cfg | sed 's/add/delete/' > mcvr.del
+cat mcvr.cfg | sed 's/add/delete/' | sed "s/$IPADDR.*$/$IPADDR/g" > mcvr.del
 cat interface.cfg | grep ipv4 | cut -d ' ' -f1-4 | sed 's/set/delete/' > interface.del
 cat vlan.cfg | sed 's/add/delete/' > vlan.del
 
-# Schritt 3: Konfigurationen entfernen. Ginge auch mit einer For-Schleife.
-echo "Entferne Proxy ARP Eintraege..."
-clish -f proxyarp.del
-echo "Entferne statische Routen..."
-clish -f staticroute.del
-echo "Entferne Cluster-VIP..."
-clish -f mcvr.del
-echo "Entferne IP-Adresse von Interface $INTERFACE..."
-clish -f interface.del
-echo "Entferne VLAN $VLAN..."
-clish -f vlan.del
-echo "Konfiguration speichern"
-clish -c "save config"
+# Schritt 3: Konfigurationen entfernen.
+read -p "Interface $INTERFACE.$VLAN entfernen? " YESNO
+if [[ $YESNO =~ ^(y|Y|yes|Yes|j|J|ja|Ja)$ ]]; then
+    echo "Entferne Proxy ARP Eintraege..."
+    clish -f proxyarp.del
+    echo "Entferne statische Routen..."
+    clish -f staticroute.del
+    echo "Entferne Cluster-VIP..."
+    clish -f mcvr.del
+    echo "Entferne IP-Adresse von Interface $INTERFACE..."
+    clish -f interface.del
+    echo "Entferne VLAN $VLAN..."
+    clish -f vlan.del
+    echo "Konfiguration speichern"
+    clish -c "save config"
+else
+    echo "Abbruch"
+fi
