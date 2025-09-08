a198addf9eae7041108a8ae03d6db5876eeaf7e0
Michael Goessmann Matos <dj0nz@users.noreply.github.com>
2025-09-08 20:06:33 +0200

    Update logwork.sh

--- logwork.sh
+++ logwork.sh
@@ -1,38 +1,50 @@
 #!/bin/bash
 
-# Firewall Logfile Cleanup
-#
-# Michael Goessmann Matos, NTT
-# Maerz 2021
+# Purpose:
+# Delete firewall log files older than $RETIME days
+# Compress them if the're older than $ZIPTIME
+# Runs scheduled on a Check Point Management or log server
+# Schedule to run daily to avoid long runtimes
 
-# Variablen
-RETIME="300"
-CP_VERSION=R80.40
+# Michael Goessmann Matos, NTT
+# Sep 2025
+
+# Vars
+RETIME="365"
+ZIPTIME="90"
+CP_VERSION=$(cat /etc/cp-release | cut -d ' ' -f4)
 EXT=log
 LOGFILE=/var/log/orange.log
 
 exec > $LOGFILE 2>&1
 
-NUM_FILES=`/usr/bin/find /var/log/opt/CPsuite-$CP_VERSION/fw1/log/ -type f -name "*.$EXT" -mtime +$RETIME -print | wc -l`
-DISK_SPACE=`df -h | grep log | awk '{print $4}'`
-
-if [ $NUM_FILES -gt 0 ]; then
-   echo "Entferne alte Check Point Logfiles"
-   echo ""
-   echo "Anzahl Dateien:    $NUM_FILES"
-   echo "Version:           $CP_VERSION"
-   echo "Retention Time:    $RETIME Tage"
-   echo "Plattenplatz Alt:  $DISK_SPACE"
-   for EXT in logptr log log_stats logaccount_ptr loginitial_ptr; do
-       echo ""
-       echo "Entferne *.$EXT Dateien"
-       /usr/bin/find /var/log/opt/CPsuite-$CP_VERSION/fw1/log/ -type f -name "*.$EXT" -mtime +$RETIME -print -exec rm {} \;
-   done
-   DISK_SPACE=`df -h | grep log | awk '{print $4}'`
-   echo ""
-   echo "Plattenplatz Neu:  $DISK_SPACE"
+NUMLOG=$(/usr/bin/find /var/log/opt/CPsuite-$CP_VERSION/fw1/log/ -type f -name "*.$EXT" -mtime +$ZIPTIME -print | wc -l)
+CURR=$(date -R | cut -d ' ' -f2,3,4,5)
+if [[ $NUMLOG =~ ^[0-9]+$ ]]; then
+    if [[ $NUMLOG -gt 0 ]]; then
+        echo "$CURR - Compressing log files older than $ZIPTIME days"
+        for EXT in logptr log log_stats logaccount_ptr loginitial_ptr; do
+            echo ""
+            echo "Compressing *.$EXT files..."
+            /usr/bin/find /var/log/opt/CPsuite-$CP_VERSION/fw1/log/ -type f -name "*.$EXT" -mtime +$ZIPTIME -print -exec gzip {} \;
+        done
+    else
+        echo "$CURR - Nothing to compress"
+    fi
 else
-   echo "Nichts zu loeschen..."
+    echo "$CURR - Something went wrong: Can't compress $NUMLOG files."
 fi
-
+    
+NUMZIP=$(/usr/bin/find /var/log/opt/CPsuite-$CP_VERSION/fw1/log/ -type f -name "*.gz" -mtime +$RETIME | wc -l)
+if [[ $NUMZIP =~ ^[0-9]+$ ]]; then
+    if [[ $NUMZIP -gt 0 ]]; then
+        CURR=$(date -R | cut -d ' ' -f2,3,4,5)
+        echo "$CURR - Deleting zipped files older than $RETIME days..."
+        /usr/bin/find /var/log/opt/CPsuite-$CP_VERSION/fw1/log/ -type f -name "*.gz" -mtime +$RETIME -print -exec rm {} \;
+    else
+        echo "$CURR - Nothing to delete"
+    fi
+else
+    echo "$CURR - Something went wrong: Can't delete $NUMZIP files."
+fi
 echo ""
